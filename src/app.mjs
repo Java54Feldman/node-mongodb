f6179b94490a9dd4c72108e9364243bfac5317f3
Pavel Feldman <feldmanpk@gmail.com>
2024-11-21 18:47:12 +0200

    fix bug in the Comments collection

--- src/app.mjs
+++ src/app.mjs
@@ -5,44 +5,49 @@ const COLLECTION_COMMENTS_NAME = "comments";
 
 const mongoConnection = new MongoConnection(process.env.MONGO_URI, DB_NAME);
 const collectionMovies = mongoConnection.getCollection(COLLECTION_MOVIES_NAME);
-const collectionComments = mongoConnection.getCollection(COLLECTION_COMMENTS_NAME);
+const collectionComments = mongoConnection.getCollection(
+  COLLECTION_COMMENTS_NAME
+);
 
 collectionComments
   .aggregate([
-    // Limit the selection to the first five documents
-    { $limit: 5 },
-
-    // Join data from the movies collection
     {
       $lookup: {
         from: "movies",
         localField: "movie_id",
         foreignField: "_id",
-        as: "movie",
+        as: "movies",
       },
     },
-
-    // Unwind the movie array, as $lookup returns an array
-    { $unwind: "$movie" },
-
-    // Replace the root document, merging fields from comments and movies
     {
-      $replaceRoot: {
-        newRoot: {
-          $mergeObjects: ["$$ROOT", { title: "$movie.title" }],
+      $match: {
+        "movies.0": {
+          $exists: true,
         },
       },
     },
 
-    // Project the required fields
+    { $limit: 5 },
+
+    {
+      $replaceRoot: {
+        newRoot: {
+          $mergeObjects: [
+            {
+              title: {
+                $arrayElemAt: ["$movies.title", 0],
+              },
+            },
+            "$$ROOT",
+          ],
+        },
+      },
+    },
+
     {
       $project: {
-        _id: 1,
-        name: 1,
-        email: 1,
-        text: 1,
-        date: 1,
-        title: 1,
+        movies: 0,
+        movie_id: 0,
       },
     },
   ])
@@ -82,7 +87,6 @@ collectionMovies
         ],
       },
     },
-    // Unwind the results
     { $unwind: "$averageRating" },
     { $unwind: "$comedies2010" },
     // Filter comedies with rating above average
@@ -93,7 +97,6 @@ collectionMovies
         },
       },
     },
-    // Project only the movie title
     {
       $project: {
         _id: 0,
